20ca6d379b39ef27da6101d90fe30f9a03881593
Marc Toensing <marc.toensing@gmail.com>
2024-05-26 21:28:56 +0200

    revert memorysize

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,26 +5,24 @@ DOCKER_USER='dockeruser'
 DOCKER_GROUP='dockergroup'
 
 if ! id "$DOCKER_USER" >/dev/null 2>&1; then
-	echo "First start of the docker container, start initialization process."
+    echo "First start of the docker container, start initialization process."
 
-	UID=${PUID:-9001}
-	GID=${PGID:-9001}
-	echo "Starting with $UID:$GID (UID:GID)"
+    USER_ID=${PUID:-9001}
+    GROUP_ID=${PGID:-9001}
+    echo "Starting with $USER_ID:$GROUP_ID (UID:GID)"
 
-	addgroup --gid $GID $DOCKER_GROUP
-	adduser $DOCKER_USER --shell /bin/sh --uid $UID --ingroup $DOCKER_GROUP --disabled-password --gecos ""
+    addgroup --gid $GROUP_ID $DOCKER_GROUP
+    adduser $DOCKER_USER --shell /bin/sh --uid $USER_ID --ingroup $DOCKER_GROUP --disabled-password --gecos ""
 
-	chown -vR $UID:$GID /opt/minecraft
-	chmod -vR ug+rwx /opt/minecraft
+    chown -vR $USER_ID:$GROUP_ID /opt/minecraft
+    chmod -vR ug+rwx /opt/minecraft
 
-	if [ "$SKIP_PERM_CHECK" != "true" ]; then
-		chown -vR $UID:$GID /data
-	fi
+    if [ "$SKIP_PERM_CHECK" != "true" ]
+    then
+        chown -vR $USER_ID:$GROUP_ID /data
+    fi
 fi
 
-# Set memory variables if not already set, falling back to MEMORYSIZE if needed
-INIT_MEMORY=${INIT_MEMORY:-${MEMORY:-${MEMORYSIZE:-1G}}}
-MAX_MEMORY=${MAX_MEMORY:-${MEMORY:-${MEMORYSIZE:-1G}}}
-
 export HOME=/home/$DOCKER_USER
-exec gosu $DOCKER_USER:$DOCKER_GROUP java -Xms$INIT_MEMORY -Xmx$MAX_MEMORY $JAVAFLAGS -jar /opt/minecraft/paperspigot.jar $PAPERMC_FLAGS nogui
+exec gosu $DOCKER_USER:$DOCKER_GROUP java -jar -Xms$MEMORYSIZE -Xmx$MEMORYSIZE $JAVAFLAGS /opt/minecraft/paperspigot.jar $PAPERMC_FLAGS nogui
+    
