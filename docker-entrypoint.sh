9996647eaa48a25ff3a6062cc845179ae5c26d57
Marc Toensing <marc.toensing@gmail.com>
2024-08-21 21:55:11 +0200

    fixes fatal: The GID `1000' is already in use.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,18 +11,26 @@ if ! id "$DOCKER_USER" >/dev/null 2>&1; then
     GROUP_ID=${PGID:-9001}
     echo "Starting with $USER_ID:$GROUP_ID (UID:GID)"
 
-    addgroup --gid $GROUP_ID $DOCKER_GROUP
-    adduser $DOCKER_USER --shell /bin/sh --uid $USER_ID --ingroup $DOCKER_GROUP --disabled-password --gecos ""
+    # Check if the desired GROUP_ID is already in use
+    if getent group $GROUP_ID >/dev/null 2>&1; then
+        EXISTING_GROUP=$(getent group $GROUP_ID | cut -d: -f1)
+        echo "GID $GROUP_ID is already in use by group $EXISTING_GROUP. Using this group."
+    else
+        addgroup --gid $GROUP_ID $DOCKER_GROUP
+        echo "Created group $DOCKER_GROUP with GID $GROUP_ID."
+    fi
 
+    # Create user with the specified UID and existing or new group
+    adduser --shell /bin/sh --uid $USER_ID --ingroup $DOCKER_GROUP --disabled-password --gecos "" $DOCKER_USER
+
+    # Set ownership and permissions
     chown -vR $USER_ID:$GROUP_ID /opt/minecraft
     chmod -vR ug+rwx /opt/minecraft
 
-    if [ "$SKIP_PERM_CHECK" != "true" ]
-    then
+    if [ "$SKIP_PERM_CHECK" != "true" ]; then
         chown -vR $USER_ID:$GROUP_ID /data
     fi
 fi
 
 export HOME=/home/$DOCKER_USER
 exec gosu $DOCKER_USER:$DOCKER_GROUP java -jar -Xms$MEMORYSIZE -Xmx$MEMORYSIZE $JAVAFLAGS /opt/minecraft/paperspigot.jar $PAPERMC_FLAGS nogui
-    
